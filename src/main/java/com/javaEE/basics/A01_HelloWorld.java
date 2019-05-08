5fc9d32e7f3044d449d691963118404ef3930718
SuperInteface <SuperInteface@A>
2019-05-08 11:36:28 +0800

    package			com.javaEE.basics java			A01_HelloWorld.java time			2019-05-08AM comment: 	hello world

---
+++ src/main/java/com/javaEE/basics/A01_HelloWorld.java
@@ -0,0 +1,16 @@
+package com.javaEE.basics;
+
+/**
+ * 第一个程序,helloWorld
+ * @author githubName: SuperInteface
+ */
+public class A01_HelloWorld {
+
+	/*
+	 * main()方法,程序的入口
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World.");
+	}
+
+}
