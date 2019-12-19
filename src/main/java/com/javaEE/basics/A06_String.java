ab6a9bc4573f14e46c0e1cb2b16d97a0b3d8f651
Superinterface <su_jia_nan@163.com>
2019-12-19 16:17:19 +0800

    提交A06_String字符串

---
+++ src/main/java/com/javaEE/basics/A06_String.java
@@ -0,0 +1,17 @@
+package com.javaEE.basics;
+
+/**
+ *	字符串
+ * @author		github : SuperInteface
+ * @createdate	2019年12月17日
+ */
+public class A06_String {
+	
+	public static void main(String[] args) {
+		
+		String s01 = new String("abc");
+//		System.arraycopy(src, srcPos, dest, destPos, length);
+		
+	}
+	
+}
