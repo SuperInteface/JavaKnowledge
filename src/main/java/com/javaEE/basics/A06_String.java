a0ab1f7cf399765d2e19154a3d5a2ef4e4e29a8e
Superinterface <su_jia_nan@163.com>
2019-12-31 14:50:28 +0800

    提交修改A06String 文件

--- src/main/java/com/javaEE/basics/A06_String.java
+++ src/main/java/com/javaEE/basics/A06_String.java
@@ -10,6 +10,24 @@ public class A06_String {
 	public static void main(String[] args) {
 		
 		String s01 = new String("abc");
+		/*
+		 *	*****String
+		 *	public final class String implements java.io.Serializable, Comparable<String>, CharSequence {
+		 *	*****String 类属性有
+		 *	char[]					value						作用:存储字符串(字符串对应的数组)
+		 *	int						hash						作用:该字符串的hash码
+		 *	long					serialVersionUID			作用:序列化uid
+		 *	ObjectStreamField[]		serialPersistentFields		作用:暂且未知
+		 *	*****String 类构造方法有
+		 *	
+		 *
+		 *
+		 */
+		
+		
+		
+		
+		
 //		System.arraycopy(src, srcPos, dest, destPos, length);
 		
 	}
